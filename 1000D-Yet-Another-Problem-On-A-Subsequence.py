cfeef71472145da3492152e2f6427876ee7b9b50
aeternalis1 <30846300+aeternalis1@users.noreply.github.com>
2018-06-27 20:36:21 -0400

    Create 1000D-Yet-Another-Problem-On-A-Subsequence.py

---
+++ 1000D-Yet-Another-Problem-On-A-Subsequence.py
@@ -0,0 +1,16 @@
+n = int(raw_input())
+nums = list(map(int,raw_input().split()))
+dp = [0 for x in range(n)]
+combs = [[0 for x in range(1001)] for x in range(1001)]
+for i in range(1001):
+  for j in range(i+1):
+    if not j:
+      combs[i][j] = 1
+    elif j:
+      combs[i][j] = combs[i-1][j]+combs[i-1][j-1]
+for i in range(n-1,-1,-1):
+  if nums[i] > 0 and nums[i]+i<n:
+    dp[i] = combs[n-i-1][nums[i]]%998244353
+    for j in range(nums[i]+i+1,n):
+      dp[i] = dp[i]+(dp[j]*combs[j-i-1][nums[i]])%998244353
+print sum(dp)%998244353
